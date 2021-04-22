9b933df54c58d6fe681b02b31a3a81b11717e3e8
bennylp <benny.prijono@gmail.com>
2021-04-23 04:03:11 +0700

    Version 2.11

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -18,7 +18,7 @@ import sys
 
 # Which pjproject tag to checkout to create the documentation.
 # Set to "master" to checkout the latest version
-pjproject_tag = '2.10'
+pjproject_tag = '2.11'
 
 # Doxygen XML files to be sanitized because it contains characters causing XML parsing to fail
 sanitize_xml_files = [
